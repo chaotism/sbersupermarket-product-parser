bcd9c39be39771991b8c025b261e3c0c297758c5
Alexandr Bogut <chaotism@mail.ru>
2023-05-15 00:56:13 +0300

    feat: add auth token validation

--- application/web/routers/__init__.py
+++ application/web/routers/__init__.py
@@ -1,9 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from web.core.auths import verify_auth_token
 
 from .api.endpoints import api_router
 from .health.endpoints import health_router
 
 base_router = APIRouter()
 
-base_router.include_router(api_router, prefix='/api')
-base_router.include_router(health_router, prefix='/health')
+base_router.include_router(
+    api_router,
+    prefix='/api',
+    dependencies=[
+        Depends(verify_auth_token),
+    ],
+)
+base_router.include_router(
+    health_router,
+    prefix='/health',
+    dependencies=[
+        Depends(verify_auth_token),
+    ],
+)
